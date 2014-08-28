b938419f7fe67ea2e06e1e9e112bfc7a2e1d4f94
Daniel Qian <chanjarster@gmail.com>
2014-08-28 16:58:28 +0800

    javadoc

--- src/main/java/chanjarster/weixin/api/WxMessageInterceptor.java
+++ src/main/java/chanjarster/weixin/api/WxMessageInterceptor.java
@@ -15,6 +15,7 @@ public interface WxMessageInterceptor {
   /**
    * 拦截微信消息
    * @param wxMessage
+   * @param context  上下文，如果handler或interceptor之间有信息要传递，可以用这个
    * @return  true代表OK，false代表不OK
    */
   public boolean intercept(WxXmlMessage wxMessage, Map<String, Object> context);
